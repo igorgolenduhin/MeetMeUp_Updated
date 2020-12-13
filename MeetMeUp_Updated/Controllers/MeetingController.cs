d4209bc72926e1977d922525c4a8309152294aa7
boldalex <46719335+boldalex@users.noreply.github.com>
2020-12-13 16:27:16 -0500

    2020-12-13

--- MeetMeUp_Updated/Controllers/MeetingController.cs
+++ MeetMeUp_Updated/Controllers/MeetingController.cs
@@ -14,9 +14,15 @@ namespace MeetMeUp_Updated.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Meeting
-        public ActionResult Meetings()
+        public ActionResult Meetings(String query)
         {
             var allMeetings = db.Meetings.ToList();
+
+            if (query != null)
+            {
+                allMeetings = allMeetings.Where(m => m.Title.ToLower().Contains(query.ToLower())).ToList();
+            }
+
             var myMeetings = allMeetings.Where(m => getMyGroupsList().Contains(m.Group)).ToList();
             return View(myMeetings);
         }
@@ -82,6 +88,38 @@ namespace MeetMeUp_Updated.Controllers
             var user = db.Users.Find(User.Identity.GetUserId());
             var allGroups = db.Groups.ToList();
             return allGroups.Where(g => g.Members.Contains(user)).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult ChangeImage(int? meetingID)
+        {
+            if (meetingID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = db.Meetings.Find(meetingID);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+        [HttpPost]
+        public ActionResult ChangeImage(HttpPostedFileBase fileUpload1, int meetingID)
+        {
+            if (fileUpload1 != null)
+            {
+                string pic = System.IO.Path.GetFileName(fileUpload1.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("/Assets/Images/MeetingPics/"), pic);
+
+                fileUpload1.SaveAs(path);
+                Meeting meeting = db.Meetings.Find(meetingID);
+                meeting.Image = "/Assets/Images/MeetingPics/" + pic;
+                db.Entry(meeting).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = meetingID });
+            }
+            return View();
         }
     }
 }
